a17a8ec1dc5bb38f4bfd48fc584703cbc20ad134
Jonathan Nogueira <LuminousPath@users.noreply.github.com>
2023-03-24 18:13:19 +0900

    feat(vagrant): added new vagrant-2.2.9 package

---
+++ lp/packages/virtualization.scm
@@ -0,0 +1,61 @@
+(define-module (lp packages virtualization)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix build-system copy)
+  #:use-module (guix build-system ruby)
+  #:use-module ((guix licenses)
+                #:prefix license:)
+  #:use-module (gnu packages ruby)
+  #:use-module (lp packages ruby))
+
+(define-public vagrant
+  (package
+    (name "vagrant")
+    (version "2.2.9")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "https://github.com/hashicorp/vagrant/archive/v" version ".tar.gz"))
+              (file-name (string-append name "-" version ".tar.gz"))
+              (sha256
+               (base32
+                "0fbickjjliaw3cpkh3pl9bp56b2gcqn87c5ag67amc450ah43rdq"))))
+    (build-system ruby-build-system)
+    (arguments
+     `(#:ruby ,ruby-2.6
+       #:tests? #f)) ; bundler error.
+    (inputs
+     `(("ruby-childprocess" ,ruby-childprocess)
+       ("ruby-erubis" ,ruby-erubis)
+       ("ruby-hashicorp-checkpoint" ,ruby-hashicorp-checkpoint)
+       ("ruby-i18n" ,ruby-i18n-1.8)
+       ("ruby-listen" ,ruby-listen-3.1)
+       ("ruby-log4r" ,ruby-log4r)
+       ("ruby-net-ssh" ,ruby-net-ssh)
+       ("ruby-net-scp" ,ruby-net-scp-1.2.0)
+       ("ruby-net-sftp" ,ruby-net-sftp)
+       ("ruby-rb-kqueue" ,ruby-rb-kqueue)
+       ("ruby-ed25519" ,ruby-ed25519)
+       ("ruby-bcrypt-pbkdf" ,ruby-bcrypt-pbkdf)
+       ("ruby-rest-client" ,ruby-rest-client)
+       ("ruby-rubyzip" ,ruby-rubyzip)
+       ("ruby-wdm" ,ruby-wdm)
+       ("ruby-winrm" ,ruby-winrm)
+       ("ruby-winrm-fs" ,ruby-winrm-fs)
+       ("ruby-winrm-elevated" ,ruby-winrm-elevated)
+       ("ruby-vagrant-cloud" ,ruby-vagrant-cloud)
+       ("ruby-ruby-dep" ,ruby-ruby-dep)
+       ))
+    (native-inputs
+     `(("bundler" ,bundler)
+       ("ruby-rake" ,ruby-rake-12)
+       ("ruby-rspec" ,ruby-rspec-3.5)
+       ("ruby-rspec-its" ,ruby-rspec-its)
+       ("ruby-webmock" ,ruby-webmock-2)
+       ("ruby-fake-ftp" ,ruby-fake-ftp)
+       ))
+    (synopsis
+     "Vagrant is a tool for building and distributing development environments.")
+    (description
+     "Vagrant is a tool for building and distributing development environments.")
+    (home-page "http://www.vagrantup.com")
+    (license license:expat)))
