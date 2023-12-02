3714f5937c6d4a1b4c013eb6b4794fd752c48c81
Jonathan Nogueira <LuminousPath@users.noreply.github.com>
2023-12-03 02:03:19 +0900

    fix(security-token.scm): fix syntax error in security token definition

--- lp/packages/security-token.scm
+++ lp/packages/security-token.scm
@@ -1,24 +1,32 @@
 (define-module (lp packages security-token)
-  (#:use-module (gnu packages python-xyz)
-   #:use-module (gnu packages python-crypto)
-   #:use-module (gnu packages security-token)))
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix build-system python)
+  #:use-module (guix build-system pyproject)
+  #:use-module (gnu packages python-xyz)
+  #:use-module (gnu packages python-crypto)
+  #:use-module (gnu packages security-token)
+  #:use-module ((guix licenses)
+                #:prefix license:))
 
-(package
-  (name "yubikey-manager")
-  (version "5.2.1")
-  (source
-   (origin
-     (method url-fetch)
-     (uri (pypi-uri "yubikey_manager" version))
-     (sha256
-      (base32 "1qg4zgrkliikwn2zh25id4wjnc9xwg07s9ik9i1x4ks7mj1smi9m"))))
-  (build-system pyproject-build-system)
-  (propagated-inputs (list python-click
-                           python-cryptography
-                           python-fido2
-                           python-keyring
-                           python-pyscard))
-  (home-page "https://github.com/Yubico/yubikey-manager")
-  (synopsis "Tool for managing your YubiKey configuration.")
-  (description "Tool for managing your @code{YubiKey} configuration.")
-  (license license:bsd-3))
+(define-public yubikey-manager
+  (package
+    (name "yubikey-manager")
+    (version "5.2.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (pypi-uri "yubikey_manager" version))
+       (sha256
+        (base32 "1qg4zgrkliikwn2zh25id4wjnc9xwg07s9ik9i1x4ks7mj1smi9m"))))
+    (build-system pyproject-build-system)
+    (propagated-inputs (list python-click
+                             python-cryptography
+                             python-fido2
+                             python-keyring
+                             python-pyscard))
+    (home-page "https://github.com/Yubico/yubikey-manager")
+    (synopsis "Tool for managing your YubiKey configuration")
+    (description "Tool for managing your @code{YubiKey} configuration.")
+    (license license:bsd-3)))
+
