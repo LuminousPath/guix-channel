e57953b7507c8f39bc549c51fe4b53bf6bfa8e5e
Jonathan Nogueira <LuminousPath@users.noreply.github.com>
2024-03-11 15:33:03 +0900

    fix(crates-extra): fix for crates being moved to crates-apple

--- lp/packages/crates-extra.scm
+++ lp/packages/crates-extra.scm
@@ -1,4 +1,5 @@
 (define-module (lp packages crates-extra)
+  #:use-module(gnu packages crates-apple)
   #:use-module (gnu packages crates-io)
   #:use-module (gnu packages crates-graphics)
   #:use-module (gnu packages crates-web)
